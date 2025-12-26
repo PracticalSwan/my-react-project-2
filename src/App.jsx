8680e0e35616a8fee1abed5fda44e35ae30b539f
Sithu <u6726077@au.edu>
2025-12-26 15:37:44 +0700

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Register from "./Register";
+import Register from "./components/Register";
 import "./App.css";
 
 export default function App() {
